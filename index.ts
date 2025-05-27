37d90508908f8500cfa0113e0572a04345cc3a56
lioarce01 <lioarce1@gmail.com>
2025-05-27 13:38:36 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import pg from "pg";
+
+const server = new Server(
+    {
+        name: "PostgreSQL MCP Server",
+        version: "1.0.0",
+        description: "A Model Context Protocol server for PostgreSQL",
+    },
+    {
+        capabilities: {
+            resources: {},
+            tools: {}
+        },
+    }
+)
+
+async function runServer()
+{
+    const transport = new StdioServerTransport();
+    await server.connect(transport)
+}
+
+runServer().catch(console.error)
